5d693ccd7393d6ade29b53124d9a87be8fbb2aab
Ellyse <ellyse@common.tools>
2025-04-24 01:05:23 +0200

    untested code to split llm response, however we should do this recursively

--- lib/langchaindemo/bot/consumer.ex
+++ lib/langchaindemo/bot/consumer.ex
@@ -4,16 +4,27 @@ defmodule Langchaindemo.Bot.Consumer do
   
   alias Nostrum.Api.Message
 
+  @discord_max_msg_length 1000
+
+  # TODO: actually should be recusive when we split because it might be more than double the max size  
   def handle_event(
         {:MESSAGE_CREATE,
          %Nostrum.Struct.Message{
            content: "!llm " <> llm_prompt,
+           channel_id: channel_id,
            author: %Nostrum.Struct.User{id: user_id, bot: nil}
-         } = msg, _ws_state}
+         } = _msg, _ws_state}
       ) do
     Logger.debug("userid #{user_id} asked question: #{llm_prompt}")
     response = Langchaindemo.doit(llm_prompt)
+    |> String.split_at(@discord_max_msg_length)
+    |> Tuple.to_list()
+    |> Enum.each(fn 
+      ""  -> :ok
+      llm_msg when is_binary(llm_msg) -> 
+        {:ok, _msg} = Message.create(channel_id, llm_msg)
+        :ok
+    end)
     Logger.debug("userid #{user_id} llm response=#{response}")
-    {:ok, _msg} = Message.create(msg.channel_id, response)
   end
 end
